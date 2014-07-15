6df42c88306cf4d60c92848b0d25dd0861864097
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-15 12:15:26 +0100

    updated deps, added tests and travis

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 var test = require("tape");
-var lib = require("../");
+var exec = require("child_process").exec;
+var fs = require("fs");
 
-test("first", function(t){
-    
+test("run grunt", function(t){
+    t.plan(1);
+    exec("grunt", function(err, stdout, stderr){
+        if (err) throw err;
+        
+        var output = fs.readFileSync("tmp/class.md", "utf8");
+        t.ok(/a class with all of the things/.test(output));
+    });
 });
