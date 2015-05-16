4e74476ee356a2612e8a324315af86d4dc2d90f8
Capelo <antonio.c.capelo@gmail.com>
2015-05-16 18:25:42 +0100

    Adding functinality for having Markdown files concatenated to the begining/end of the generated documentation - missing files

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,21 @@ module.exports = function(grunt) {
                 src: "test/fixture/*.js",
                 dest: "test/output/docs.md"
             },
+            oneOutputFileWithExtraMarkdown: {
+                src: "test/fixture/*.js",
+                dest: "test/output/docsWithExtraMarkdown.md",
+                options: {
+                    "introMd": "test/begin.md",
+                    "endMd": "test/end.md"
+                }
+            },
             separateOutputFiles: {
                 files: [
                     { src: "test/fixture/class.js", dest: "test/output/class.md" },
                     { src: "test/fixture/typedef.js", dest: "test/output/typedef.md" }
                 ]
             },
+
             withOptions: {
                 options: {
                     "no-gfm": true
--- tasks/jsdoc2md.js
+++ tasks/jsdoc2md.js
@@ -1,6 +1,7 @@
 "use strict";
 var jsdoc2md = require("jsdoc-to-markdown");
 var mfs = require("more-fs");
+var concat = require("concat-files");
 
 module.exports = function(grunt) {
     grunt.registerMultiTask("jsdoc2md", "API documentation generator", function() {
@@ -10,10 +11,31 @@ module.exports = function(grunt) {
         var monitor = {
             finished: 0,
             done: function(){
-                this.finished++;
-                if (this.finished === self.files.length) done();
+                monitor.finished++;
+                if (monitor.finished === self.files.length) done();
             }
         };
+
+        function concatMD(concatOptions, cb) {
+            var files = [];
+            if (concatOptions.intro) {
+                files.push(concatOptions.intro); 
+            }
+
+            files.push(concatOptions.outputPath);
+
+            if (concatOptions.end) {
+                files.push(concatOptions.end); 
+            }
+
+            mfs.mkdir('.concat-tmp');
+            concat(files, '.concat-tmp/temp.md', function() {
+                mfs.duplicate('.concat-tmp/temp.md', concatOptions.outputPath);
+                mfs.deleteFile('.concat-tmp/temp.md');
+                mfs.rmdir('.concat-tmp');
+                cb();
+            });
+        }
 
         this.files.forEach(function(file){
             var outputPath = file.dest;
@@ -22,7 +44,17 @@ module.exports = function(grunt) {
             grunt.log.oklns("writing " + outputPath);
             renderStream.on("error", grunt.fail.fatal);
             renderStream.on("end", function(){
-                monitor.done();
+                console.log(options);
+                if (options.introMd || options.endMd) {
+                    var concatOptions = {
+                        intro: options.introMd,
+                        outputPath: outputPath,
+                        end: options.endMd
+                    };
+                    concatMD(concatOptions, monitor.done); 
+                } else {
+                    monitor.done(); 
+                }    
             });
 
             /* dest directories will be created if they don't exist  */
